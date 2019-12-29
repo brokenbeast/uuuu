3d7ed1cfd8b751ffe4bc441f6f52a1cb1c96fe1b
brokenbeast <13466366075@163.com>
2019-12-29 18:26:44 +0800

    good

--- hunter/hunter.ino
+++ hunter/hunter.ino
@@ -9,24 +9,35 @@ void loop() {
   delay(1000);
   for(counter = 2;counter<= 8;counter++){
      //语句将被执行10次
-     DigiKeyboard.print("3");
+     DigiKeyboard.print("8");
      DigiKeyboard.print("9");
      DigiKeyboard.delay(1000);
-     DigiKeyboard.print("3");
+     DigiKeyboard.print("8");
      DigiKeyboard.print("9");
      DigiKeyboard.delay(1000);
-     DigiKeyboard.print("3");
+     DigiKeyboard.print("8");
      DigiKeyboard.print("9");
      DigiKeyboard.delay(1000);
-     DigiKeyboard.print("3");
+     DigiKeyboard.print("8");
      DigiKeyboard.print("9");
      DigiKeyboard.delay(1000);
      DigiKeyboard.delay(1000);
+     DigiKeyboard.print("3");
+     DigiKeyboard.delay(1000);
+     DigiKeyboard.print("1");
+     DigiKeyboard.delay(1000);
+     DigiKeyboard.print("5");
+     DigiKeyboard.delay(500);
+     DigiKeyboard.print("9");
      DigiKeyboard.print("1");
      DigiKeyboard.delay(5500);
+     DigiKeyboard.print("6");
+     DigiKeyboard.delay(500);
      DigiKeyboard.print("9");
      DigiKeyboard.delay(500);
-     DigiKeyboard.print("1");
+     DigiKeyboard.print("6");
+     DigiKeyboard.delay(3300);
+     DigiKeyboard.print("9");
      
   }
   // put your main code here, to run repeatedly:
